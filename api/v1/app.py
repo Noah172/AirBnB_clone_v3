6c8a6deb2df88d931c5193b80779961042626dad
oscargomez87 <oscarstiven87@gmail.com>
2020-04-28 12:56:25 -0500

    Added status page using flask blueprint Returns json response

---
+++ api/v1/app.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""displays app status"""
+from flask import Flask
+from models import storage
+from api.v1.views import app_views
+from os import getenv
+app = Flask(__name__)
+app.register_blueprint(app_views, url_prefix='/api/v1')
+
+
+@app.teardown_appcontext
+def close_session(resp_or_excep):
+    print(app.url_map)
+    storage.close()
+
+
+if __name__ == '__main__':
+    if getenv('HBNB_API_HOST'):
+        app.run(host=getenv('HBNB_API_HOST'),
+                port=getenv('HBNB_API_PORT'), threaded=True)
+    else:
+        app.run(host='0.0.0.0', threaded=True)
