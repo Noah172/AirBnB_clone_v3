01353cee1d17f8b8d2bae9fa47727de9ca72a2f7
oscargomez87 <oscarstiven87@gmail.com>
2020-04-29 14:49:33 -0500

    Enabled CORS to the resource /* with origin 0.0.0.0

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 """displays app status"""
 from flask import Flask, jsonify
+from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
 from os import getenv
 app = Flask(__name__)
 app.register_blueprint(app_views, url_prefix='/api/v1')
-
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 @app.teardown_appcontext
 def close_session(resp_or_excep):
