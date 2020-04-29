831afecb1bd7e005b852197c8a3b41266fdfb28c
noah172 <jsebastian.garcia1995@gmail.com>
2020-04-29 15:49:23 +0000

    added states

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """Creates base Blueprint"""
 from flask import Blueprint
+
 app_views = Blueprint('/api/v1', __name__)
 from api.v1.views.index import *
+from api.v1.views.states import *
 from api.v1.views.cities import *
